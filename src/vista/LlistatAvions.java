d44c8650811bc69a12ad797388210a1036fc09a9
Xavier Franch <xavi.franchtarazaga@gmail.com>
2019-04-01 16:06:46 +0200

    LlistatAvions DONE LlistatCompanyies DONE

--- src/vista/LlistatAvions.java
+++ src/vista/LlistatAvions.java
@@ -1,5 +1,6 @@
 package vista;
 
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTable;
@@ -20,20 +21,31 @@ public class LlistatAvions {
 
     /*
     CONSTRUCTOR
-    Paràmetres:cap
+    Paràmetres:capx
     Accions:
     Heu d'inicialitzar els atributs d'aquesta classe fent el següent (no afegiu cap listener a cap control):
-            
+
     - Heu d'inicialitzar l'objecte JFrame amb títol "Llistat d'avions" i layout Grid d'una columna
     - Heu d'inicialitzar l'objecte Jtable amb un nou objecte de TaulaAvio
     - Heu d'inicialitzar l'objecte JButton amb nom "Sortir".
     - Heu d'afegir-ho tot a l'atribut frame
     - Heu de fer visible el frame amb l'amplada i alçada que proposen els atributs alcada i amplada
     - Heu de fer que la finestra es tanqui quan l'usuari ho fa amb el control "X" de la finestra
-       
+
      */
     public LlistatAvions() {
+      frame = new JFrame("Llistat d'avions");
+      frame.setLayout(new GridLayout(0, 1));
 
+      taulaAvions = new JTable();
+      bSortir = new JButton("Sortir");
+
+      frame.add(taulaAvions);
+      frame.add(bSortir);
+
+      frame.setSize(AMPLADA, ALCADA);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
     }
 
     public JFrame getFrame() {
